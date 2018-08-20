e0f13d578143d1a82d6f20b7cc73fe756da9bc9f
Baiyuan Wang <bywang@thoughtworks.com>
2018-08-20 14:02:41 +0800

    fix Participant's phone range

--- src/model/Participant.js
+++ src/model/Participant.js
@@ -6,7 +6,7 @@ export default class Participant {
   static fromString(str) {
     const array = str.split(/\t+|\s+/, 2);
     if(array[0] && array[1]) {
-      if (!(/^[1][3,4,5,7,8][0-9]{9}$/.test(array[1]))) {
+      if (!(/^[1][3-9]{10}$/.test(array[1]))) {
         throw new Error(`'${array[0]}'的手机号有误: 请输入11位数字`);
       }
       return new Participant(array[0], array[1]);
