97a3a14b18f94994ef221eb187a2824e2387c5d3
Baiyuan Wang <bywang@thoughtworks.com>
2019-05-28 14:00:58 +0800

    fix unable to copy paste

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,38 @@
 const electron = require('electron');
+const package = require('../package');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const menu = electron.Menu;
-menu.setApplicationMenu(null);
+menu.setApplicationMenu(menu.buildFromTemplate([
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'delete' },
+      { role: 'selectall' }
+    ]
+  },
+  {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Submit feedback...',
+        click: function () {
+          electron.shell.openExternal(package.bugs.url);
+        }
+      },
+      {
+        role: 'about',
+      },
+    ]
+  }
+]));
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
@@ -10,9 +40,10 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({ width: 1000, height: 800 });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.setAutoHideMenuBar(true);
 }
 
 app.on('ready', createWindow);
