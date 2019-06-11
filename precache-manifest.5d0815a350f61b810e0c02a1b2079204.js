80d52e3884d39f12e0f4dbd262fde338468d8bc6
Baiyuan Wang <bywang@thoughtworks.com>
2019-06-11 16:23:40 +0800

    Updates

---
+++ precache-manifest.5d0815a350f61b810e0c02a1b2079204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "622bb7a19e3c21858bded50c80ed5f50",
+    "url": "/static/media/bg.622bb7a1.jpeg"
+  },
+  {
+    "revision": "aab060a4b44d6b68e229dd5c63b0d016",
+    "url": "/static/media/albird.aab060a4.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ddd912c4f6759a4e98d",
+    "url": "/static/js/main.6ddd912c.chunk.js"
+  },
+  {
+    "revision": "ef68cab630c741b5d684",
+    "url": "/static/js/1.ef68cab6.chunk.js"
+  },
+  {
+    "revision": "6ddd912c4f6759a4e98d",
+    "url": "/static/css/main.1cbb590a.chunk.css"
+  },
+  {
+    "revision": "2ea6523bbf7e72aa68aa337b1e4fe15f",
+    "url": "/index.html"
+  }
+];
