18ec8140c247145ec07a3137f7f971282fbd275f
Baiyuan Wang <bywang@thoughtworks.com>
2019-06-11 16:58:05 +0800

    Updates

---
+++ electron.js
@@ -0,0 +1,63 @@
+const electron = require('electron');
+const { autoUpdater } = require("electron-updater")
+const package = require('../package');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const menu = electron.Menu;
+menu.setApplicationMenu(menu.buildFromTemplate([
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'delete' },
+      { role: 'selectall' }
+    ]
+  },
+  {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Submit feedback...',
+        click: function () {
+          electron.shell.openExternal(package.bugs.url);
+        }
+      },
+      {
+        role: 'about',
+      },
+    ]
+  }
+]));
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 1000, height: 800 });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.setAutoHideMenuBar(true);
+  autoUpdater.checkForUpdatesAndNotify();
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ precache-manifest.4b0c2f41ceca7bedd4e848d610636873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b039bf29805a0882c12aadc3bbdb61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21efed9deb5c12364db2",
+    "url": "./static/css/main.4ca1e351.chunk.css"
+  },
+  {
+    "revision": "f4d9c0707936cfbbd21a",
+    "url": "./static/js/2.e9958cee.chunk.js"
+  },
+  {
+    "revision": "21efed9deb5c12364db2",
+    "url": "./static/js/main.9c3380e5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "aab060a4b44d6b68e229dd5c63b0d016",
+    "url": "./static/media/albird.aab060a4.svg"
+  },
+  {
+    "revision": "622bb7a19e3c21858bded50c80ed5f50",
+    "url": "./static/media/bg.622bb7a1.jpeg"
+  }
+]);
