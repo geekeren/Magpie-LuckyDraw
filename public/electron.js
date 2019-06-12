7229714db84460e7ff7929f0876182fa57b73eca
Baiyuan Wang <bywang@thoughtworks.com>
2019-06-12 11:52:57 +0800

    Add splash screen

--- public/electron.js
+++ public/electron.js
@@ -41,7 +41,7 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 1000, height: 800 });
+  mainWindow = new BrowserWindow({ width: 1024, height: 768 });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
   mainWindow.setAutoHideMenuBar(true);
